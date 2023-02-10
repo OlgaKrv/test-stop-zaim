d290b9646a68936fffc534bfd3dff79071859dfc
Olga <ola.korotkova95@gmail.com>
2023-02-10 06:50:25 +0500

    tea shop project initialization

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app" class="project-wrapper"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import Vue from "vue"
+import App from "./App.vue"
+import './assets/tailwind.css'
+
+import store from "./store"
+import './assets/tailwind.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  store,
+  render: h => h(App),
+}).$mount("#app");
